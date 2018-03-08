3eca7afd310f10817d0ed749d2a591897a589f81
Abstractize <gababarca@hotmail.com>
2018-03-08 00:06:37 -0600

    First List created

--- src/ADT/List.java
+++ src/ADT/List.java
@@ -1,5 +1,29 @@
 package ADT;
 
 public class List {
+	//Atributos
+	private Nodo head;
+	private int length;
+	//Constructor
+	public List(){
+		setHead(null);
+		setLength(0);
+	}
+	//Getters y Setters
+	public Nodo getHead() {
+		return head;
+	}
 
+	public void setHead(Nodo head) {
+		this.head = head;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	//Métodos
 }
--- src/ADT/Nodo.java
+++ src/ADT/Nodo.java
@@ -1,5 +1,28 @@
 package ADT;
 
 public class Nodo {
+	private int value;
+	private Nodo next;
+	
+	public Nodo(){
+		this.setValue(0);
+		this.setNext(null);
+	}
 
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Nodo getNext() {
+		return next;
+	}
+
+	public void setNext(Nodo next) {
+		this.next = next;
+	}
+	//Referencia: http://codigolibre.weebly.com/blog/listas-simples-en-java
 }
